43a5ad6b5545c12b7215407d25b1d6682437c117
Fabian David Romero Jimenez <fabian.romero.jimenez@gmail.com>
2026-07-14 15:55:45 -0500

    updated Strategy user config query

--- users-config-backend-java/src/main/java/com/stamina/usersconfig/strategy/repository/StrategyConfigRepository.java
+++ users-config-backend-java/src/main/java/com/stamina/usersconfig/strategy/repository/StrategyConfigRepository.java
@@ -2,11 +2,14 @@ package com.stamina.usersconfig.strategy.repository;
 
 import com.stamina.usersconfig.strategy.entity.StrategyConfig;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.UUID;
 
 public interface StrategyConfigRepository extends JpaRepository<StrategyConfig, UUID> {
     List<StrategyConfig> findBySymbolAndEnabledTrue(String symbol);
+    
+    @Query("SELECT s FROM StrategyConfig s JOIN FETCH s.user WHERE s.user.id = :userId")
     List<StrategyConfig> findByUserId(UUID userId);
 }
