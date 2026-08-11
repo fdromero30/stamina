9ae3e292715d591f9640731fbfb474b7e668cc32
Fabian David Romero Jimenez <fabian.romero.jimenez@gmail.com>
2026-08-10 20:44:40 -0500

    updated nginx render

--- frontend-react/entrypoint.sh
+++ frontend-react/entrypoint.sh
@@ -7,11 +7,6 @@ else
   TEMPLATE=/etc/nginx/templates/nginx.local.conf
 fi
 
-USERS_CONFIG_API_URL_HOST=$(echo "${USERS_CONFIG_API_URL}" | sed -E 's|https?://([^/]+)/?.*|\1|')
-TRADING_CORE_API_URL_HOST=$(echo "${TRADING_CORE_API_URL}" | sed -E 's|https?://([^/]+)/?.*|\1|')
-
-envsubst \
-  '${PORT} ${USERS_CONFIG_API_URL} ${TRADING_CORE_API_URL} ${USERS_CONFIG_API_URL_HOST} ${TRADING_CORE_API_URL_HOST}' \
-  < "$TEMPLATE" > /etc/nginx/conf.d/default.conf
+envsubst '${PORT} ${USERS_CONFIG_API_URL} ${TRADING_CORE_API_URL}' < "$TEMPLATE" > /etc/nginx/conf.d/default.conf
 
 exec nginx -g 'daemon off;'
